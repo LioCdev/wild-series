54b9950cb437e62bb1d34ed9599c03088655145a
LioCdev <lionel.choppin@gmail.com>
2021-11-30 22:20:15 +0100

    added Category Controller and associated view

---
+++ src/Controller/CategoryController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Category;
+use App\Entity\Program;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @route("/category", name = "category_")
+ * 
+ */
+class CategoryController extends AbstractController
+{
+    /**
+     * @Route("/", name = "index")
+     * @return Response
+     */
+    public function index(): Response
+    {
+        $categoryList = $this->getDoctrine()->getRepository(Category::class)->findAll();
+
+        return $this->render('category/index.html.twig', [
+            'controller_name' => 'CategoryController',
+            'categoryList' => $categoryList]);
+    }
+
+    /**
+     * @route("/{categoryName}", name = "show")
+     * @return Response
+     */
+    public function show(string $categoryName): Response
+    {
+        $category = $this->getDoctrine()->getRepository(Category::class)->findOneBy(['name' => $categoryName]);
+
+        if (!$category) {
+            // throw $this->createNotFoundException(
+            //     'No program with id : '.$categoryName.' found in program\'s table.'
+            // );
+            return $this->render('/404.html.twig');
+        } 
+
+        $categoryContent = $this->getDoctrine()->getRepository(Program::class)->findBy(['category' => $category], ['id' => 'DESC']);
+        return $this->render('category/show.html.twig', ['categoryContent' => $categoryContent, 'category' => $category]);
+    }
+}
--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -2,6 +2,7 @@
 // src/Controller/ProgramController.php
 namespace App\Controller;
 
+use App\Entity\Program;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,19 +14,32 @@ class ProgramController extends AbstractController
 {
     /**
      * @Route("/", name="index")
+     * @return Response
      */
     public function index(): Response
     {
+        $programs = $this->getDoctrine()->getRepository(Program::class)->findAll();
         return $this->render('program/index.html.twig', [
             'website' => 'Wild Séries',
+            'programs' => $programs
         ]);
     }
 
     /**
-     * @route("/{id}/", methods={"GET"}, requirements={"id"="\d+"}, name="show") 
+     * @route("/show/{id}/", methods={"GET"}, requirements={"id"="\d+"}, name="show")
+     * @return Response
      */
     public function show(int $id): Response
     {
-        return $this->render('program/show.html.twig', ['id' => $id]);
+        $program = $this->getDoctrine()
+        ->getRepository(Program::class)
+        ->findOneBy(['id' => $id]);
+
+        if (!$program) {
+            throw $this->createNotFoundException(
+                'No program with id : '.$id.' found in program\'s table.'
+            );
+        }
+        return $this->render('program/show.html.twig', ['program' => $program]);
     }
 }
