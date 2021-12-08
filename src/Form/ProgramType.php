d74f0f246573972feb9c28fd772bf199422de329
LioCdev <lionel.choppin@gmail.com>
2021-12-08 21:43:27 +0100

    add actors field on program/new form

--- src/Form/ProgramType.php
+++ src/Form/ProgramType.php
@@ -2,8 +2,9 @@
 
 namespace App\Form;
 
+use App\Entity\Actor;
 use App\Entity\Program;
-use SebastianBergmann\Type\NullType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
@@ -11,6 +12,7 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\UrlType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+
 
 class ProgramType extends AbstractType
 {
@@ -23,7 +25,13 @@ class ProgramType extends AbstractType
             ->add('country', TextType::class)
             ->add('year', IntegerType::class)
             ->add('category', null, ['choice_label' => 'name'])
-        ;
+            ->add('actors', EntityType::class, [
+                'class' => Actor::class,
+                'choice_label' => 'name',
+                'multiple' => true,
+                'expanded' => true,
+                'by_reference' => false,
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
